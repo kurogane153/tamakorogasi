a9c256b4e78a2d701d4ea1a47cb365cdf710da67
kurogane153 <11807067@stu.kbc.ac.jp>
2021-10-27 14:01:48 +0900

    ボールが壁に当たった時正反射するようにした

---
+++ Assets/kurogane/Script/AddPower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPower : MonoBehaviour
+{
+    private Rigidbody rb;
+    private BounceCalc bounceCalc;
+    public float power = 1;    // 発射時の力
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        bounceCalc = this.GetComponent<BounceCalc>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 発射時のvelocityを取得
+        bounceCalc.afterReflectVero = rb.velocity;
+    }
+}
